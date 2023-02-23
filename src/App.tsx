741052956cbafc8fabebe42571e384ea985ac4d8
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2023-02-23 10:33:48 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import "./App.css";
+
+const Calculator = () => {
+  const [display, setDisplay] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value: string) => {
+    if (value === "=") {
+      try {
+        setResult(eval(display));
+      } catch (error) {
+        setResult("Error");
+      }
+      setDisplay("");
+    } else if (value === "C") {
+      setDisplay("");
+      setResult("");
+    } else {
+      setDisplay(display + value);
+    }
+  };
+
+  return (
+    <>
+      <div className="container-calculator">
+        <div className="box-calculator">
+          <input type="text" value={display} />
+          <div className="btn-calculator">
+
+            {/* Números */}
+            <button onClick={() => handleButtonClick("1")}>1</button>
+            <button onClick={() => handleButtonClick("2")}>2</button>
+            <button onClick={() => handleButtonClick("3")}>3</button>
+            <button onClick={() => handleButtonClick("4")}>4</button>
+            <button onClick={() => handleButtonClick("5")}>5</button>
+            <button onClick={() => handleButtonClick("6")}>6</button>
+            <button onClick={() => handleButtonClick("7")}>7</button>
+            <button onClick={() => handleButtonClick("8")}>8</button>
+            <button onClick={() => handleButtonClick("9")}>9</button>
+            <button onClick={() => handleButtonClick("0")}>0</button>
+
+            {/* Operadores */}
+            <button className="operator" onClick={() => handleButtonClick("/")}>
+              /
+            </button>
+            <button className="operator" onClick={() => handleButtonClick("-")}>
+              -
+            </button>
+            <button className="operator" onClick={() => handleButtonClick("*")}>
+              *
+            </button>
+            <button className="operator" onClick={() => handleButtonClick("+")}>
+              +
+            </button>
+            <button className="clear" onClick={() => handleButtonClick("C")}>
+              C
+            </button>
+            <button className="equal" onClick={() => handleButtonClick("=")}>
+              =
+            </button>
+
+          </div>
+          {result === "Error" ? (
+            <p className="error">Essa operação não pode ser realizada.</p>
+          ) : (
+            <p className="results">{result}</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Calculator;
